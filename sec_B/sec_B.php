744c07d8e5561ed2b2584eb3013ad06b3e84f926
Ashutosh Kumar Maurya <47826975+ashutosh-maurya0@users.noreply.github.com>
2023-09-29 21:00:18 +0530

    Required Field (*)

--- sec_B/sec_B.php
+++ sec_B/sec_B.php
@@ -30,9 +30,9 @@
             <h1>Section B form</h1>
             <div class="mb-3">
                 <div class="policy">
-                    <label for="policyQuestion" class="form-label">Whether your entity’s policy/policies cover each principle & core elements of National Guidelines On Responsible Business Conduct(NGRBC):</label>
+                    <label for="policyQuestion" class="form-label">Whether your entity’s policy/policies cover each principle & core elements of National Guidelines on responsible Business conduct(NGRBC):</label>
                 </div>
-                <select class="form-select" id="policyQuestion" name="policyQuestion" required>
+                <select class="form-select" id="policyQuestion" name="policyQuestion">
                     <option value="">Select an option</option>
                     <option value="Yes">Yes</option>
                     <option value="No">No</option>
@@ -48,7 +48,7 @@
 
             <div class="mb-3">
                 <div class="policy">
-                    <label for="procedureQuestion" class="form-label">Whether the entity has translated the policy into procedures:</label>
+                    <label for="procedureQuestion" class="form-label"><span style="color: red;"> * </span>Whether the entity has translated the policy into procedures:</label>
                 </div>
                 <select class="form-select" id="procedureQuestion" name="procedureQuestion" required>
                     <option value="">Select an option</option>
@@ -66,7 +66,7 @@
 
             <div class="mb-3">
                 <div class="policy">
-                    <label for="valueChainQuestion" class="form-label">Do the enlisted policies extend to your value chain partners?:</label>
+                    <label for="valueChainQuestion" class="form-label"><span style="color: red;"> * </span>Do the enlisted policies extend to your value chain partners?:</label>
                 </div>
                 <select class="form-select" id="valueChainQuestion" name="valueChainQuestion" required>
                     <option value="">Select an option</option>
@@ -86,33 +86,33 @@
                 <div class="policy">
                     <label for="standardsQuestion" class="form-label">Name of the national & international codes/certifications/labels/standards adopted by your entity & mapped to each principle:</label>
                 </div>
-                <textarea class="form-control" id="standardsQuestion" name="standardsQuestion" rows="4" required></textarea>
+                <textarea class="form-control" id="standardsQuestion" name="standardsQuestion" rows="4" ></textarea>
             </div>
 
             <div class="mb-3">
                 <div class="policy">
                     <label for="commitmentsQuestion" class="form-label">Specific commitments, goals & targets set by the entity with defined timelines, if any:</label>
                 </div>
-                <textarea class="form-control" id="commitmentsQuestion" name="commitmentsQuestion" rows="4" required></textarea>
+                <textarea class="form-control" id="commitmentsQuestion" name="commitmentsQuestion" rows="4" ></textarea>
             </div>
 
             <div class="mb-3">
                 <div class="policy">
-                    <label for="performanceQuestion" class="form-label">Performance of the entity against the specific commitments, goals & targets along with reasons in case the same are not met:</label>
+                    <label for="performanceQuestion" class="form-label"><span style="color: red;"> * </span>Performance of the entity against the specific commitments, goals & targets along with reasons in case the same are not met:</label>
                 </div>
                 <textarea class="form-control" id="performanceQuestion" name="performanceQuestion" rows="4" required></textarea>
             </div>
 
             <div class="mb-3">
                 <div class="policy">
-                    <label for="directorStatement" class="form-label">Statement by director responsible for the business responsibility report, highlighting Environmental, Social, Governance(ESG) related challenges, targets & achievements:</label>
+                    <label for="directorStatement" class="form-label"><span style="color: red;"> * </span>Statement by director responsible for the business responsibility report, highlighting ESG related challenges, targets & achievements:</label>
                 </div>
                 <textarea class="form-control" id="directorStatement" name="directorStatement" rows="4" required></textarea>
             </div>
 
             <div class="mb-3">
                 <div class="policy">
-                    <label for="achievementsQuestion" class="form-label">Achievements (Upload PDF only - Max size 5 MB)</label>
+                    <label for="achievementsQuestion" class="form-label"><span style="color: red;"> * </span>Achievements (Upload PDF only Max 5 MB)</label>
                 </div>
                 <input type="file" id="file" name="uploadedFiles[]" accept=".pdf" required multiple>
                 <br><br>
@@ -120,14 +120,14 @@
 
             <div class="mb-3">
                 <div class="policy">
-                    <label for="authorityDetails" class="form-label">Details of the highest authority responsible for implementation & oversight of the Business Responsibility policy:</label>
+                    <label for="authorityDetails" class="form-label"><span style="color: red;"> * </span>Details of the highest authority responsible for implementation & oversight of the Business Responsibility policy:</label>
                 </div>
                 <textarea class="form-control" id="authorityDetails" name="authorityDetails" rows="4" required></textarea>
             </div>
 
             <div class="mb-3">
                 <div class="policy">
-                    <label for="sustainabilityCommitteeQuestion" class="form-label">Does the entity have a specified Committee of the Board/Director responsible for decision-making on sustainability-related issues?</label>
+                    <label for="sustainabilityCommitteeQuestion" class="form-label"><span style="color: red;"> * </span>Does the entity have a specified Committee of the Board/Director responsible for decision-making on sustainability-related issues?</label>
                 </div>
                 <select class="form-select" id="sustainabilityCommitteeQuestion" name="sustainabilityCommitteeQuestion" required>
                     <option value="">Select an option</option>
@@ -147,7 +147,7 @@
                 <div class="policy">
                     <label for="assessmentQuestion" class="form-label">Has the entity carried out independent assessment/evaluation of the working of its policies by an external agency?</label>
                 </div>
-                <select class="form-select" id="assessmentQuestion" name="assessmentQuestion" required>
+                <select class="form-select" id="assessmentQuestion" name="assessmentQuestion" >
                     <option value="">Select an option</option>
                     <option value="Yes">Yes</option>
                     <option value="No">No</option>
