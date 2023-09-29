b256d19893d6d15e8084d1648ec71adcf6eff7fc
Ashutosh Kumar Maurya <47826975+ashutosh-maurya0@users.noreply.github.com>
2023-09-29 20:54:58 +0530

    Email validation and required fields

--- sec_A/sec_A.php
+++ sec_A/sec_A.php
@@ -21,7 +21,6 @@
       <ul class="main-nav">
         <li><a href="">Home</a></li>
         <li><a href="#">About</a></li>
-        <li><a href="sec_C.php">Section C</a></li>
         <li><a href="#">Contact</a></li>
       </ul>
     </header>
@@ -29,17 +28,18 @@
       <h1>Section A form</h1>
       <form id="primary" enctype="multipart/form-data">
         <div class="form-group">
-          <label for="cin">Corporate Identity Number (CIN)</label>
-          <input type="text" id="cin" name="cin" class="form-control" placeholder="Enter CIN of Listed Entity" required> 
+          <label for="cin"><span style="color: red;"> * </span>Corporate Identity Number (CIN)</label>
+          <input type="text" id="cin" name="cin" class="form-control" placeholder="Enter CIN of Listed Entity" required>
+          
         </div>
 
         <div class="form-group">
-          <label for="name">Name</label>
+          <label for="name"><span style="color: red;"> * </span>Name</label>
           <input type="text" id="name" name="name" class="form-control" placeholder="Enter name of Listed Entity" required>
         </div>
 
         <div class="form-group">
-          <label for="inyear">Incorporation Year</label>
+          <label for="inyear"><span style="color: red;"> * </span>Incorporation Year</label>
           <select name="year" id="inyear" class="form-control"></select>
           <script>
             var start = 2022;
@@ -53,49 +53,49 @@
         </div>
 
         <div class="form-group">
-          <label for="office_add">Registered Office Address</label>
+          <label for="office_add"><span style="color: red;"> * </span>Registered Office Address</label>
           <input type="text" id="office_add" name="office_address" class="form-control" placeholder="Enter registered address" required>
         </div>
 
         <div class="form-group">
-          <label for="corp_add">Corporate Address</label>
+          <label for="corp_add"><span style="color: red;"> * </span>Corporate Address</label>
           <input type="text" id="corp_add" name="corporate_address" class="form-control" placeholder="Enter Corporate address" required>
         </div>
 
         <div class="form-group">
-          <label for="email">Email</label>
+          <label for="email"><span style="color: red;"> * </span>Email</label>
           <input type="text" id="email" name="email" class="form-control" placeholder="Enter your email" required>
           <p id="validationMessage"></p>
-      <script>
-          const emailInput = document.getElementById("email");
-          const validationMessage = document.getElementById("validationMessage");
-          emailInput.addEventListener("input", function() {
-          const email = emailInput.value;
-          const gmailRegex = /@gmail\.com$/;
-          if (gmailRegex.test(email)) {
-              validationMessage.textContent = "Valid email!";
-              validationMessage.style.color = "green";
-          } else {
-              validationMessage.textContent = "Invalid email ID";
-              validationMessage.style.color = "red";
-          }
-          });
-      </script>
+            <script>
+              const emailInput = document.getElementById("email");
+              const validationMessage = document.getElementById("validationMessage");
+              emailInput.addEventListener("input", function() {
+              const email = emailInput.value;
+              const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+              if (emailRegex.test(email)) {
+                validationMessage.textContent = "Valid email!";
+                validationMessage.style.color = "green";
+              } else {
+                  validationMessage.textContent = "Invalid email ID";
+                  validationMessage.style.color = "red";
+              }
+              });
+           </script>
         </div>
 
         <div class="form-group">
-          <label for="contact">Telephone</label>
+          <label for="contact"><span style="color: red;"> * </span>Telephone</label>
           <input type="text" id="contact" name="telephone" class="form-control" placeholder="Enter your contact number" required>
         </div>
 
 
         <div class="form-group">
-          <label for="website">Website</label>
+          <label for="website"><span style="color: red;"> * </span>Website</label>
           <input type="url" id="website" name="website" class="form-control" placeholder="Enter your website link" required>
         </div>
 
         <div class="form-group">
-          <label for="year">Reporting Financial year</label>
+          <label for="year"><span style="color: red;"> * </span>Reporting Financial year</label>
           <select name="reporting_fin_year" id="year" class="form-control"></select>
           <script>
             var start = 2022;
@@ -110,69 +110,69 @@
 
         <div class="mb-3">
           <div class="policy">
-            <label for="incorporation_certificate">Upload Incorporation Certificate (Upload PDF only - Max size 5 MB)</label>
+            <label for="incorporation_certificate"><span style="color: red;"> * </span>Please provide your incorporation certificate as a PDF file (Max 5 MB)</label>
           </div>
           <input type="file" id="file" name="uploadedFiles[]" accept=".pdf" required multiple><br><br>
         </div>
 
         <div class="form-group">
-          <label for="se-name">Name of the Stock Exchange(s)</label>
+          <label for="se-name"><span style="color: red;"> * </span>Name of the Stock Exchange(s)</label>
           <input type="text" id="se-name" name="name_stock" class="form-control" placeholder="Enter name of the Stock Exchange(s) where shares are listed" required>
         </div>
 
         <div class="form-group">
-          <label for="puc">Paid-up Capital</label>
+          <label for="puc"><span style="color: red;"> * </span>Paid-up Capital</label>
           <input type="text" id="puc" name="puc" required pattern="[0-9]+">
         </div>
 
-        <!-- Deatils of POC-->
+        <!-- Details of POC -->
         <div class="form-group">
           <p class="poc">
-            <label for="details">Details of Point of Contact</label><br>
-            <label for="poc-name">Name</label>
-            <input type="text" id="poc-name" name="poc_name" placeholder="Enter the name of person to contact for queries">
-            <label for="poc-phone">Phone Number</label>
+            <label for="details"><span style="color: red;"> * </span>Details of Point of Contact</label><br>
+            <label for="poc-name"><span style="color: red;"> * </span>Name</label>
+            <input type="text" id="poc-name" name="poc_name" placeholder="Enter the name of the person to contact for queries" required>
+            <label for="poc-phone"><span style="color: red;"> * </span>Phone Number</label>
             <input type="text" id="poc-phone" name="poc_phone" required pattern="[0-9]+" placeholder="Enter the contact number">
-            <label for="email">Email Address</label>
+            <label for="poc-email"><span style="color: red;"> * </span>Email Address</label>
             <input type="text" id="poc-email" name="poc_email" class="form-control" placeholder="Enter email address" required>
-            <p id="validationMessage"></p>
+            <p id="poc-validationMessage">Enter a valid email address.</p> <!-- Unique validation message ID -->
             <script>
-                const emailInput = document.getElementById("email");
-                const validationMessage = document.getElementById("validationMessage");
-                emailInput.addEventListener("input", function() {
-                const email = emailInput.value;
-                const gmailRegex = /@gmail\.com$/;
-                if (gmailRegex.test(email)) {
-                    validationMessage.textContent = "Valid email!";
-                    validationMessage.style.color = "green";
+              const pocEmailInput = document.getElementById("poc-email"); // Unique ID
+              const pocValidationMessage = document.getElementById("poc-validationMessage"); // Unique ID
+              pocEmailInput.addEventListener("input", function() {
+                const email = pocEmailInput.value;
+                const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+                if (emailRegex.test(email)) {
+                  pocValidationMessage.textContent = "Valid email!";
+                  pocValidationMessage.style.color = "green";
                 } else {
-                    validationMessage.textContent = "Invalid email ID";
-                    validationMessage.style.color = "red";
+                  pocValidationMessage.textContent = "Invalid email ID";
+                  pocValidationMessage.style.color = "red";
                 }
-                });
+              });
             </script>
           </p>
         </div>
-        <!-- Deatils of POC ends-->
+        <!-- Details of POC ends -->
         <div class="form-group">
           <label for="rep-b">Reporting boundary</label>
-          <input type="text" id="rep-b" name="rep_b" class="form-control" placeholder="Enter Reporting boundary" required>
+          <input type="text" id="rep-b" name="rep_b" class="form-control" placeholder="Enter Reporting boundary" >
         </div>
         <div class="form-group">
           <label for="doba">Details of Business activities</label>
-          <textarea id="doba" name="doba" rows="5" cols="50" class="form-control" placeholder="Enter details of Business activities of the entity (accounting for 90% of the entity’s Turnover)" required></textarea>
+          <textarea id="doba" name="doba" rows="5" cols="50" class="form-control" placeholder="Enter details of Business activities of the entity (accounting for 90% of the entity’s Turnover)"></textarea>
         </div>
         <div class="form-group">
           <label for="dops">Details of the products & services</label>
-        <textarea id="dops" name="dops" rows="5" cols="50" class="form-control" placeholder="Enter details of the products & services sold by the entity (accounting for 90% of the entity’s Turnover)" required></textarea>
+        <textarea id="dops" name="dops" rows="5" cols="50" class="form-control" placeholder="Enter details of the products & services sold by the entity (accounting for 90% of the entity’s Turnover)" ></textarea>
         </div>
         <div class="form-group">
           <label for="nol">Number of locations</label>
-          <input type="text" id="nol" name="nol" required pattern="[0-9]+" placeholder=" Enter number of locations where plants/operation/ offices of the entity are located.">
+          <input type="text" id="nol" name="nol" placeholder=" Enter number of locations where plants/operation/ offices of the entity are located.">
         </div>
         <div class="form-group">
           <label for="drm">Details regarding markets served by the entity.</label>
-          <textarea id="drm" name="drm" rows="5" cols="50" class="form-control" placeholder="Enter details regarding markets served by the entity." required></textarea>
+          <textarea id="drm" name="drm" rows="5" cols="50" class="form-control" placeholder="Enter details regarding markets served by the entity." ></textarea>
         </div>
 
         <!-- Deatils of employee-->
@@ -196,11 +196,11 @@
         </div>
         <!-- Deatils of employee ends-->
         <div class="form-group">
-          <label for="p-no">Participation/Inclusion/Representation of Women</label>
+          <label for="p-no"><span style="color: red;"> * </span>Participation/Inclusion/Representation of Women</label>
           <input type="text" id="p-no" name="p_no" required pattern="[0-9]+" placeholder=" Enter number of Female members of the BOD/KMP">
         </div>
         <div class="form-group">
-          <label for="tor">Turnover rate for permanent employees & workers (For past 3 years)</label>
+          <label for="tor"><span style="color: red;"> * </span>Turnover rate for permanent employees & workers (For past 3 years)</label>
           <input type="text" id="tor" name="tor" required pattern="[0-9]+" placeholder="Enter Turnover rate for past 3 years">
         </div>
         <!-- Deatils of corporate structure-->
@@ -212,25 +212,25 @@
             <input type="text" id="holding" name="holding" placeholder="Enter the details of Holding">
             
             <label for="subsidiary">Subsidiary</label>
-            <input type="text" id="subsidiary" name="subsidiary" required pattern="[0-9]+" placeholder="Enter the subsidiary">
+            <input type="text" id="subsidiary" name="subsidiary" placeholder="Enter the subsidiary">
             
             <label for="ac">Associate Companies (including joint ventures)</label>
-            <textarea id="ac" name="ac" rows="5" cols="50" class="form-control" placeholder="Enter the details of Associate Companies (including joint ventures)" required></textarea>  
+            <textarea id="ac" name="ac" rows="5" cols="50" class="form-control" placeholder="Enter the details of Associate Companies (including joint ventures)" ></textarea>  
           </p>
         </div>
         <!-- Deatils of corporate structure ends-->
         <div class="form-group">
-          <label for="csr">Details of Corporate Social Responsibility(CSR) Activities </label>
-          <textarea id="csr" name="csr" rows="5" cols="50" class="form-control" placeholder="Enter the details of CSR Activities" required></textarea>  
+          <label for="csr">Details of CSR Activities </label>
+          <textarea id="csr" name="csr" rows="5" cols="50" class="form-control" placeholder="Enter the details of CSR Activities" ></textarea>  
         </div>
 
         <div class="form-group">
-          <label for="gre">Grievance redressal </label>
+          <label for="gre"><span style="color: red;"> * </span>Grievance redressal </label>
           <textarea id="gre" name="gre" rows="5" cols="50" class="form-control" placeholder=" Raise concerns about, & seek remedy for, the organization’s potential & actual negative impacts on them" required></textarea>  
         </div>
 
         <div class="form-group">
-          <label for="overview">Overview of the entity’s material responsible business conduct issues </label>
+          <label for="overview"><span style="color: red;"> * </span>Overview of the entity’s material responsible business conduct issues </label>
           <textarea id="overview" name="overview" rows="5" cols="50" class="form-control" placeholder="Increased awareness & understanding of climate risks & opportunities within the entity resulting in better risk management & more informed strategic planning" required></textarea>  
         </div>
 
